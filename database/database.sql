067ac2fee95a7beb08425f1edff1473ed5fdc881
aprok89 <atsuouchiha@gmail.com>
2026-09-16 20:26:25 +0200

    Kolejny Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -18,3 +18,35 @@ CREATE TABLE users (
 
     CONSTRAINT uq_users_email UNIQUE (email)
 )
+ ENGINE=InnoDB;
+
+CREATE INDEX idx_users_role ON users (role);
+
+CREATE TABLE service_categories (
+    id            INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(100) NOT NULL,
+    description   TEXT NULL,
+
+    CONSTRAINT uq_category_name UNIQUE (name)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE services (
+    id            INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    category_id   INT UNSIGNED NOT NULL,
+    name          VARCHAR(100) NOT NULL,
+    description   TEXT NULL,
+    duration      SMALLINT UNSIGNED NOT NULL COMMENT 'czas trwania w minutach',
+    price         DECIMAL(8,2) NOT NULL,
+    active        TINYINT(1)   NOT NULL DEFAULT 1,
+
+    CONSTRAINT fk_services_category
+        FOREIGN KEY (category_id) REFERENCES service_categories(id)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+
+    CONSTRAINT chk_services_duration CHECK (duration > 0),
+    CONSTRAINT chk_services_price CHECK (price >= 0)
+) ENGINE=InnoDB;
+
+CREATE INDEX idx_services_category ON services (category_id);
+CREATE INDEX idx_services_active ON services (active);
